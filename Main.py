b2d3048f562952844d4c32b51dff73a0c8d52886
URK21CS2009 Davis Alex <109649381+Davis-cloud@users.noreply.github.com>
2022-10-11 21:35:17 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
 
 class Queue:
@@ -12,32 +16,3 @@ class Queue:
     else:
         self.last.next = Node(data)
         self.last = self.last.next
-
-  def dequeue(self) -> None:
-    if self.head is None:
-            return None
-    else:
-        to_return = self.head.data
-        self.head = self.head.next
-        return to_return
-
-  def status(self) -> None:
-    temp = self.head
-    while temp!= None:
-      print(temp.data, "=>", sep="", end = "")
-      temp = temp.next
-    print("None")
-
-# Do not change the following code
-queue = Queue()
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = input_data.split(',')
-for i in range(len(operations)):
-  if operations[i] == "enqueue":
-    queue.enqueue(int(data[i]))
-  elif operations[i] == "dequeue":
-    queue.dequeue()
-queue.status()
